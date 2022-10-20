12f0408e91e134ac9e49c54332c755ae099f7262
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2022-10-19 20:43:18 -0700

    updated directory

--- directory.ts
+++ directory.ts
@@ -1,63 +1,22 @@
 import "dotenv/config";
-import axios from "axios";
+// import axios from "axios";
 import { User } from "./interfaces";
+import { ds } from "aserto-node";
 
-const authorizerServiceUrl = process.env.ASERTO_AUTHORIZER_SERVICE_URL;
-const tenantId = process.env.ASERTO_TENANT_ID;
-const authorizerApiKey = process.env.ASERTO_AUTHORIZER_API_KEY;
-
-// get a user's profile from the management API
-const getUser: (string) => Promise<User> = async (userId) => {
-  try {
-    const url = `${authorizerServiceUrl}/api/v1/dir/users/${userId}?fields.mask=id,display_name,picture,email`;
-
-    const response = await axios({
-      method: "get",
-      url,
-      headers: {
-        Authorization: `basic ${authorizerApiKey}`,
-        "aserto-tenant-id": tenantId,
-        "Content-Type": "application/json",
-      },
-    });
-
-    // console.log("RESULT", response);
-
-    const result: User = response.data?.result;
-    return result;
-  } catch (error) {
-    console.error(`getUser: caught exception: ${error}`);
-    return null;
-  }
-};
-
-const getUserIdByUserSub: (string) => Promise<string> = async (userSub) => {
-  try {
-    const url = `${authorizerServiceUrl}/api/v1/dir/identities`;
-    const response = await axios({
-      method: "post",
-      url,
-      headers: {
-        Authorization: `basic ${authorizerApiKey}`,
-        "aserto-tenant-id": tenantId,
-        "Content-Type": "application/json",
-      },
-      data: JSON.stringify({
-        identity: userSub,
-      }),
-    });
-
-    const result: string = response.data?.id;
-    return result;
-  } catch (error) {
-    console.error(`getUser: caught exception: ${error}`);
-    return null;
-  }
-};
+const dir = ds(process.env.CA_FILE);
 
 const getUserByUserID: (string) => Promise<User> = async (userSub) => {
-  const userId = await getUserIdByUserSub(userSub);
-  return await getUser(userId);
+  const user = await dir.object({
+    type: "user",
+    key: userSub,
+  });
+  const { email, picture } = user.properties;
+  return {
+    id: user.id,
+    name: user.displayName,
+    email,
+    picture,
+  };
 };
 
 export { getUserByUserID };
